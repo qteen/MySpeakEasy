6f0147a162a42b72d12a05a1aa7d539a73d16ac8
qteen <qteen4j@gmail.com>
2016-08-01 13:55:25 +0700

    hapus salah comment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('@risingstack/trace');
 // your application's code
-// var express = require('express');
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
