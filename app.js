b00586c9e52f89271670fdb09a0d90c3e0adf3ec
qteen <qteen4j@gmail.com>
2016-08-03 14:52:26 +0700

    v8-profiler,pmx

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('@risingstack/trace');
 require('newrelic');
+var pmx = require('pmx').init({
+  http          : true, // HTTP routes logging (default: true)
+  ignore_routes : [/socket\.io/, /notFound/], // Ignore http routes with this pattern (Default: [])
+  errors        : true, // Exceptions loggin (default: true)
+  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network       : true, // Network monitoring at the application level
+  ports         : true  // Shows which ports your app is listening on (default: false)
+});
 // your application's code
 var express = require('express');
 var path = require('path');
